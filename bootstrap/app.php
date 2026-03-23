9f1c499c72d85ab025cd06f2a44ce134fc9f1695
miriamjuarez1321-max <miriamjuarez1321@gmail.com>
2026-03-22 23:14:19 -0600

    componiendo la pagina
    se descompuso

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'block.direct' => \App\Http\Middleware\BlockDirectAccess::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
